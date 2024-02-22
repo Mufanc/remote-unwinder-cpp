e4a230680d52da28f69a8f67a81934aea31a6585
Mufanc <mufanc.xyz@gmail.com>
2024-02-22 19:53:43 +0800

    feat: unwind specific pid

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,35 @@ using namespace std::literals;
     }
 }
 
-int main() {
+
+void run_demo() {
     int child_pid = fork();
 
     if (child_pid == 0) {
         some_func();
     } else {
         auto ru = RemoteUnwinder(child_pid);
+
+        ru.Attach();
+
+        AndroidUnwinderData data;
+
+        ru.Unwind(data);
+        ru.Detach();
+
+        for (const auto& frame : data.frames) {
+            LOGI("%s", ru.FormatFrame(frame).c_str());
+        }
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        run_demo();
+    } else {
+        int pid = (int) std::strtol(argv[1], nullptr, 10);
+        auto ru = RemoteUnwinder(pid);
 
         ru.Attach();
 
